ce244f5d10413a7ebf15905a5473b668f7142b5e
Alexey Anasov <webdev.alexey94@yandex.by>
2022-09-03 18:44:18 +0300

    m2_l2 add

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const modalTitle = document.querySelector('.add-product__title');
+console.log(modalTitle);
+const productId = document.querySelector('.add-product__id');
+console.log(productId);
+const buttonId = document.querySelector('.add-product__id-button');
+console.log(buttonId);
+const productForm = document.querySelector('.add-product__form');
+console.log(productForm);
+
+const checkbox = document.querySelector(".add-product__checkbox");
+console.log(checkbox);
+const checkInput = document.querySelectorAll(".add-product__input")[3];
+console.log(checkInput);
+const sumPrice = document.querySelectorAll('.card-sum__price')[1];
+console.log(sumPrice);
