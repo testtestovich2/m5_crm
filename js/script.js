6de26fba29e4d71ded1fa099747b20fa851eb6c8
Alexey Anasov <webdev.alexey94@yandex.by>
2022-09-04 14:19:59 +0300

    add m5-l7

--- js/script.js
+++ js/script.js
@@ -2,24 +2,29 @@
 
 const addProduct = document.querySelector('.list-product__table-add');
 const tBody = document.querySelector('.list-product__table-body');
+const allProductSum = document.querySelector('.list-product .card-sum__price');
 
-const modalTitle = document.querySelector('.add-product__title');
-const productId = document.querySelector('.add-product__id');
-const buttonId = document.querySelector('.add-product__id-button');
 const formOverlay = document.querySelector('.overlay');
-const productForm = document.querySelector('.add-product__form');
+const modalTitle = formOverlay.querySelector('.add-product__title');
+const productId = formOverlay.querySelectorAll('.add-product__id')[1];
+const buttonId = formOverlay.querySelector('.add-product__id-button');
+const productSum = formOverlay.querySelector('.card-sum__price');
 
-const checkbox = document.querySelector(".add-product__checkbox");
-const checkInput = document.querySelectorAll(".add-product__input")[3];
-const sumPrice = document.querySelectorAll('.card-sum__price')[1];
+const productForm = formOverlay.querySelector('.add-product__form');
+const inputId = productForm.querySelectorAll('.add-product__input')[0];
+const checkbox = productForm.querySelector(".add-product__checkbox");
+const checkInput = productForm.querySelectorAll(".add-product__input")[4];
 
-const tableBody = document.querySelector('.list-product__table-body');
+let discont;
 
 checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
     checkInput.disabled = false;
+    checkInput.focus();
   } else {
     checkInput.disabled = true;
+    checkInput.value = '';
+    discont = null;
   }
 });
 
@@ -80,27 +85,44 @@ const data = [
       "big": "img/lan_proconnect43-3-25-b.jpg"
     }
   }
-]
-const createRow = ({id, title, category, units, count, price}) => {
-  
-  // const tr = document.createElement('tr');
-  // tr.classList.add('list-product__table-tr');
-  // const tdId = document.createElement('td');
-  // tdId.textContent = id;
-  // const tdTitle = document.createElement('td');
-  // tdTitle.textContent = title;
-  // const tdCategory = document.createElement('td');
-  // tdCategory.textContent = category;
-  // const tdUnits = document.createElement('td');
-  // tdUnits.textContent = units;
-  // const tdCount = document.createElement('td');
-  // tdCount.textContent = count;
-  // const tdPrice = document.createElement('td');
-  // tdPrice.textContent = price;
+];
 
-  // tr.append(tdId, tdTitle, tdCategory, tdUnits, tdCount, tdPrice);
-  // return tr;
+const addProductData = product => {
+  data.push(product);
+};
 
+const setDiscount = (discont) => (discont ? (100 - discont) / 100 : 1);
+
+const getTotal = (price, count, discont) => {
+  if (price <= 0 || count <= 0) return 0
+  return price * count * setDiscount(discont)
+};
+
+const getTotalTable = (prices = []) => {
+  return prices.reduce(
+    (acc, { count, price, discont }) => acc + getTotal(price, count, discont),
+    0
+  )
+};
+
+const isNumber = (num) => {
+  return !isNaN(parseFloat(num)) && isFinite(num) ? +num : null
+};
+
+const newTotalSum = () => {
+  allProductSum.textContent = `$ ${getTotalTable(data).toFixed(2)}`;
+};
+newTotalSum();
+
+productForm.addEventListener('change', () => {
+  const price = isNumber(productForm.querySelector('[name=price]').value)
+  const count = isNumber(productForm.querySelector('[name=count]').value)
+  //const discont = isNumber(productForm.querySelector('[name=discont]').value)
+  productSum.textContent = `$ ${getTotal(price, count, discont).toFixed(2)}`;
+});
+
+
+const createRow = ({id, title, category, units, count, price, discont}) => {
   const tr = document.createElement('tr'); 
   tr.classList.add('list-product__table-tr'); 
   tr.insertAdjacentHTML('afterbegin', `
@@ -110,7 +132,11 @@ const createRow = ({id, title, category, units, count, price}) => {
     <td class=list-product__table-td>${units}</td>
     <td class=list-product__table-td>${count}</td>
     <td class=list-product__table-td>$${price}</td>
-    <td class=list-product__table-td>$${count*price}</td>
+    <td class=list-product__table-td>$${getTotal(
+      price,
+      count,
+      discont
+    ).toFixed(2)}</td>
     <td class=list-product__table-td>
       <button class='list-product__table-button list-product__button-img' aria-label="image"></button>
       <button class='list-product__table-button list-product__button-edit' aria-label="edit"></button>
@@ -125,79 +151,114 @@ const renderGoods = (elem, data) => {
 
   elem.append(...allRow); 
 };
+renderGoods(tBody, data);
 
-renderGoods(tableBody, data);
+const openModal = () => {
+  formOverlay.classList.add('overlay_disabled');
+};
+const closeModal = () => {
+  discont = null;
+  formOverlay.classList.remove('overlay_disabled');
+  checkInput.disabled = true;
+  productSum.textContent = '$ 0.00';
+  productId.contentEditable = 'false';
+  productForm.reset();
+};
 
 addProduct.addEventListener('click', () => {
-  formOverlay.classList.add('overlay_disabled');
+  productId.textContent = `${Date.now().toString().slice(4)}`;
+  openModal();
 });
-
-// const handleForm = () => {
-//   formOverlay.classList.toggle("overlay_disabled");
-// };
-// const toggleForm = (e) => {
-//   if (formOverlay.classList.contains("overlay_disabled")) {
-//     if (!e.target.closest(".add-product")) {
-//       handleForm();
-//     } else if (e.target.classList.contains("add-product__button-close")) {
-//       e.preventDefault();
-//       handleForm();
-//     }
-//   } else if (e.target.closest(".add-product")) {
-//     handleForm();
-//   }
-// };
-// formOverlay.addEventListener("click", toggleForm);
 
 formOverlay.addEventListener('click', (e) => {
   const target = e.target;
   if (target === formOverlay || target.classList.contains('add-product__button-close')) {
-    formOverlay.classList.remove('overlay_disabled');
+    closeModal();
   }
 });
 
-  tBody.addEventListener('click', (e) => {
-    if (e.target.closest('.list-product__button-delete')) {
-      data.splice([...document.querySelectorAll('.list-product__button-delete')]
-      .indexOf(e.target), 1);
-      e.target.closest('.list-product__table-tr').remove();
-    }
-    console.log(data);
-  }); 
-  
+tBody.addEventListener('click', (e) => {
+  if (e.target.closest('.list-product__button-delete')) {
+    data.splice([...document.querySelectorAll('.list-product__button-delete')]
+    .indexOf(e.target), 1);
+    e.target.closest('.list-product__table-tr').remove();
+    newTotalSum();
+  }
+  console.log(data);
+});   
 console.log(data);
-// const myData = {
-//   productName: "",
-//   productCount: "",
-//   productCategory: "",
-//   productPrice: "",
 
-//   check: function (v) {
-//     let e = +prompt(v);
-//     if (!Number.isNaN(e)) {
-//       return e;
-//     } else {
-//       alert("Вы ввели некорректные данные!");
-//       return this.check(v);
-//     }
-//   },
+buttonId.addEventListener('click', () => {
+  productId.contentEditable = 'true';
+  productId.focus();
+});
 
-//   question: function () {
-//     this.productName = prompt("Наименование товара");
-//     this.productCount = this.check("Количество товара");
-//     this.productCategory = prompt("Категория товара");
-//     this.productPrice = this.check("Цена товара");
+const addProductPage = (product, tBody) => {
+  tBody.append(createRow(product))
+};
+ 
+// productForm.addEventListener('submit', e => {
+//   e.preventDefault();
+//   inputId.value = +productId.textContent;
 
-//     console.log(`Наименование товара: ${this.productName}`);
-//     console.log(`Количество товара: ${this.productCount}`);
-//     console.log(`Категория товара: ${this.productCategory}`);
-//     console.log(`Цена товара: ${this.productPrice}`);
-//     console.log(
-//       `На складе ${this.productCount} единицы товара "${
-//         this.productName
-//       }" на сумму ${this.productPrice * this.productCount} деревянных`
-//     );
-//   },
-// };
+//   const formData = new FormData(e.target);
+//   const newProduct = Object.fromEntries(formData);
 
-// myData.question();
+//   console.log('newProduct: ', newProduct);
+
+//   addProductPage(newProduct, tBody);
+//   addProductData(newProduct);
+
+//   closeModal();
+//   newTotalSum();
+// });
+
+const convertPromo = (promo) => {  
+  //promo = promo.toUpperCase();
+  
+  if (promo === 'METHED') {   
+    discont = 15;  
+  } else   
+  if (promo === 'NEWYEAR') {   
+    discont = 5; 
+  } else {   
+    discont = ''; 
+    checkInput.value = '';
+  }   
+};
+
+checkInput.addEventListener('change', (e) => {
+  e.target.value = e.target.value.toUpperCase();
+  convertPromo(e.target.value)
+});
+
+const handleAddProduct = (e) => {
+  e.preventDefault()
+
+  const product = {};
+  checkInput.value = discont
+  inputId.value = +productId.textContent;
+  const formData = new FormData(productForm);
+
+  formData.forEach((value, key) => {
+    if (key !== 'discount-checkbox') {
+      product[key] =
+        key === 'price' || key === 'count' || key === 'discont'
+          ? +value
+          : value
+    };
+  });
+  if (product.description.trim() === '') return;
+  if (!('discont' in product)) {
+    product.discont = false
+  };
+
+  console.log(product);
+  addProductData(product);
+  addProductPage(product, tBody);
+  
+  newTotalSum();
+  closeModal();
+};
+
+productForm.addEventListener('submit', handleAddProduct);
